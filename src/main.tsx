c4a3e86aa3e665d46c61d261c04a0cd48039a887
yale1995 <yale850@gmail.com>
2022-09-12 16:07:04 -0300

    feat: user interface and fake api

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 3,
+          title: "Motel",
+          type: "withdraw",
+          category: "Diversão",
+          amount: 100,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
